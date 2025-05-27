649e633ccb7d48bcfa7ccfa494d09da1da9cfffd
HizkiaPappang <hiskiapappang@gmail.com>
2025-05-27 09:26:49 +0800

    ubah

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-import sklearn
 from sklearn.neighbors import NearestNeighbors
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
